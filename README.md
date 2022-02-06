70b1e41bef9314d06fc561d3580f9432e37ace11
ultraviolet10 <aritrac1998@hotmail.com>
2022-02-06 19:58:14 +0530

    Added lever-components files

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# lever-components
+## Lever-Components
+
+Lever Components repo is a collection of styles, javascript, and elm components that are used across all of Lever's frontend properties.
+
+## Sample App
+
+This repository is mostly used as a pure component library, but there is a very basic sample app for helping test connect Eth.
+
+```
---
+++ src/elm/CompoundComponents/Eth/Network.elm
@@ -0,0 +1,238 @@
+module CompoundComponents.Eth.Network exposing (Network(..), getEtherscanDomain, networkFromId, networkFromName, networkId, networkName)
+
+-- From https://ethereum.stackexchange.com/a/17101
+
+
+type Network
+    = Olympic
+    | MainNet
+    | Morden
+    | Ropsten
+    | Rinkeby
+    | Goerli
+    | Ubiq
+    | Kovan
+    | Sokol
+    | Core
+    | Development
+    | Velas
+    | Polygon
+    | Unknown
+
+
+networkFromId : Int -> Network
+networkFromId networkIdVal =
+    case networkIdVal of
+        0 ->
+            Olympic
+
+        1 ->
+            MainNet
+
+        2 ->
+            Morden
+
+        3 ->
+            Ropsten
+
+        4 ->
+            Rinkeby
+
+        5 ->
+            Goerli
+
+        8 ->
+            Ubiq
+
+        42 ->
+            Kovan
+
+        77 ->
+            Sokol
+
+        99 ->
+            Core
+
+        111 ->
+            Velas
+
+        999 ->
+            Development
+
+        1337 ->
+            Development
+
+        80001 -> 
+            Polygon
+
+        _ ->
+            Unknown
+
+
+networkFromName : String -> Network
+networkFromName name =
+    case String.toLower name of
+        "mainnet" ->
+            MainNet
+
+        "ropsten" ->
+            Ropsten
+
+        "goerli" ->
+            Goerli
+
+        "kovan" ->
+            Kovan
+
+        "rinkeby" ->
+            Rinkeby
+
+        "development" ->
+            Development
+
+        "velas" ->
+            Velas
+        
+        "polygon" ->
+            Polygon
+
+        _ ->
+            MainNet
+
+
+networkName : Network -> String
+networkName network =
+    case network of
+        Olympic ->
+            "Olympic"
+
+        MainNet ->
+            "Mainnet"
+
+        Morden ->
+            "Morden"
+
+        Ropsten ->
+            "Ropsten"
+
+        Rinkeby ->
+            "Rinkeby"
+
+        Goerli ->
+            "Goerli"
+
+        Ubiq ->
+            "Ubiq"
+
+        Kovan ->
+            "Kovan"
+
+        Sokol ->
+            "Sokol"
+
+        Core ->
+            "Core"
+
+        Development ->
+            "Development"
+
+        Velas ->
+            "Velas"
+        
+        Polygon ->
+            "Polygon"
+
+        Unknown ->
+            "unknown"
+
+
+networkId : Network -> Int
+networkId network =
+    case network of
+        Olympic ->
+            0
+
+        MainNet ->
+            1
+
+        Morden ->
+            2
+
+        Ropsten ->
+            3
+
+        Rinkeby ->
+            4
+
+        Goerli ->
+            5
+
+        Ubiq ->
+            8
+
+        Kovan ->
+            42
+
+        Sokol ->
+            77
+
+        Core ->
+            99
+        
+        Velas ->
+            111
+
+        Development ->
+            999
+        
+        Polygon ->
+            80001
+
+        Unknown ->
+            9999
+
+
+getEtherscanDomain : Network -> Maybe String
+getEtherscanDomain network =
+    case network of
+        Olympic ->
+            Nothing
+
+        MainNet ->
+            Just "etherscan.io"
+
+        Morden ->
+            Nothing
+
+        Ropsten ->
+            Just "ropsten.etherscan.io"
+
+        Rinkeby ->
+            Just "rinkeby.etherscan.io"
+
+        Goerli ->
+            Just "goerli.etherscan.io"
+
+        Ubiq ->
+            Nothing
+
+        Kovan ->
+            Just "kovan.etherscan.io"
+
+        Sokol ->
+            Nothing
+
+        Core ->
+            Nothing
+
+        Development ->
+            Nothing
+
+        Velas ->
+            Just "evmexplorer.testnet.velas.com"
+
+        Polygon ->
+            Just "https://mumbai.polygonscan.com/"
+
+        Unknown ->
+            Nothing
